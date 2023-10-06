408e31e3df6322c9c64546ddea968472db34cf39
Haifeng <jiandandemingzi@gmail.com>
2023-10-06 16:22:34 +0200

    Update hipoteca.py

--- hipoteca.py
+++ hipoteca.py
@@ -28,22 +28,23 @@ def cal_roi(ints, years, perc, value, alq, gas, jove, vpo):
 	st.write('El valor de piso:', value)
 	st.write('Pago inicial:', ent)
 	st.write('Pago mensual:', month_payment)
+	st.write('Impuesto Inmobiliario anual*:', ibi * 12)
 	st.write('Si podrías alquilarlo por ', alq, ' euros/mes, el ROI anual será: ', (roi * 100), '%')
 
 
 #cal_roi(1.35, 30, 90, 150000, 800)
-col7, col8= st.beta_columns([8,2])
+col7, col8= st.columns([8,2])
 with col7:
-	value = st.number_input("Valor de piso (euro): ", 100000, 500000, 220000, 10000)
+	value = st.number_input("Valor de piso (euro): ", 70000, 1100000, 220000, 10000)
 	value = int(value)
 with col8:
 	perc = st.selectbox("Porcentaje de hipoteca (%): ", ('40%', '50%', '60%', '70%', '80%', '90%', '95%', '100%'))
 	perc = int(perc[:-1])
 
 
-col1, col2= st.beta_columns([7,3])
+col1, col2= st.columns([7,3])
 with col1:
-	ints = st.number_input('Tipo de interés (%):', 0.01, 3.00, 0.89, 0.1)
+	ints = st.number_input('Tipo de interés (%):', 0.01, 10.00, 0.89, 0.1)
 with col2:
 	age = st.number_input('Edad:', 18, 54, 30, 1)
 if age > 32:
@@ -54,7 +55,7 @@ duration = min([70 - age, 40])
 x = min([duration, 30])
 years = st.slider('Duración de hipoteca (años): ', 10, duration, x)
 
-alq = st.slider('Ingresos de alquiler esperados (euros/mes): ', 400, 2000, 950, 50)
+alq = st.slider('Ingresos de alquiler esperados (euros/mes): ', 400, 4000, 950, 50)
 
 gas = st.text_input('Gastos (euros/mes): ', 100)
 
@@ -63,6 +64,11 @@ if vpo:
 	vpo = 1
 else:
 	vpo = 0
+first = st.checkbox('Primer piso')
+if first:
+	jove = jove
+else:
+	jove = 0
 
 if st.button('Simular'):
 	cal_roi(ints, years, perc, value, alq, int(gas), jove, vpo)
